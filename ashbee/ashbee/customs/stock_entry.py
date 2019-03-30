db9c22496b6f2d7f8785c9169de7a8be2ab2f7cb
Brown-Harry Boma <brownharryb@gmail.com>
2019-03-30 21:29:01 +0100

    Update

--- ashbee/ashbee/customs/stock_entry.py
+++ ashbee/ashbee/customs/stock_entry.py
@@ -1,5 +1,77 @@
+import json
 import frappe
+from frappe.utils import flt
+from erpnext.controllers.item_variant import get_variant, create_variant
 
+def printmsg(msglist):
+	print("=="*8)
+	print("\n"*8)
+	for i in msglist:
+		print(i)
+	print("\n"*8)
+	print("=="*8)
+
+@frappe.whitelist()
+def create_multiple_variants(**filters):
+	variants = []
+	for k,v in filters.items():
+		if k == "cmd":
+			continue
+		v = json.loads(v);
+		if not isinstance(v, dict):
+			continue
+		variant = create_variant_item(**v)
+		variants.append({"cdn":v.get('cdn'),"variant":variant})
+	return variants
+
+
+
+@frappe.whitelist()
+def create_variant_item(**filters):
+	if not filters.get("item_code") or not filters.get("attr_type") or not filters.get("attr_value"):
+		return
+	item = frappe.get_doc("Item", filters.get("item_code"))
+	if not item.variant_of:
+		return
+	args = {filters.get('attr_type'):filters.get("attr_value")}
+	variant = get_variant(item.variant_of, args)
+	if variant:
+		variant = frappe.get_doc("Item", variant)
+	else:
+		variant = create_variant(item.variant_of, args)
+		variant.valuation_rate = 0.0
+	if filters.get('valuation_rate'):
+		variant.valuation_rate = filters.get('valuation_rate')
+	variant.save();
+	return variant
+
+
+@frappe.whitelist()
+def get_finished_variant_item(**filters):
+	if not filters.get("item_code") or not filters.get("attr_type") or not filters.get("attr_value"):
+		return
+	item = frappe.get_doc("Item", filters.get("item_code"))
+	if not item.variant_of:
+		return
+	variant = get_variant(item.variant_of, {filters.get("attr_type"):filters.get("attr_value")})
+	if variant:
+		variant = frappe.get_doc("Item", variant)
+		return {'name':variant.name, 'rate':variant.valuation_rate}
+
+
+
+
+@frappe.whitelist()
+def get_attribute_values(**filters):
+	if not filters.get("item_code") or not filters.get("attr_type"):
+		return []
+	item = frappe.get_doc("Item", filters.get("item_code"))
+	if not item.variant_of:
+		return []
+	attr_type = filters.get("attr_type")
+	return frappe.db.sql('''select attribute_value from `tabItem Attribute Value`
+							where parent = '{parent}' and parenttype = 'Item Attribute'; '''
+							.format(parent=attr_type),as_list = 1)
 
 @frappe.whitelist()
 def get_variant_items(doctype, txt, searchfield, start, page_len, filters, as_dict=False):
@@ -43,7 +115,7 @@ def get_issue_items(**kwargs):
 		if entry_item.ashbee_finished_item:
 			finished_item = frappe.get_doc("Item", entry_item.ashbee_finished_item)
 			new_entry_item.item_code = finished_item.item_code
-			new_entry_item.valuation_rate = finished_item.valuation_rate
+			new_entry_item.valuation_rate = flt(entry_item.ashbee_finished_item_valuation)
 			new_entry_item.stock_uom = finished_item.stock_uom
 		new_entry_item.basic_rate = new_entry_item.valuation_rate * new_entry_item.qty
 		data.append(new_entry_item)
--- ashbee/public/js/stock_entry.js
+++ ashbee/public/js/stock_entry.js
@@ -1,5 +1,197 @@
 
+var ashbee_finished_item_populate = function(frm){
+	frm.set_query("ashbee_finished_item","items", function(doc,cdt,cdn) {
+			var child = locals[cdt][cdn];
+				return {
+					query:"ashbee.ashbee.customs.stock_entry.get_variant_items",
+					filters: {
+						'item_code':child.item_code,
+						'attr_type':child.ashbee_attribute_type
 
+					}
+				};
+			});
+}
+
+var ashbee_attribute_values_populate = function(frm, cdt, cdn){
+	var child = locals[cdt][cdn];
+	if(child.ashbee_recipient_task != ""){
+			frappe.call({
+			method:"ashbee.ashbee.customs.stock_entry.get_attribute_values",
+			args:{'item_code':child.item_code, 'attr_type':child.ashbee_attribute_type},
+			callback:function(r){
+				var m = [];
+				$(r.message).each(function(i,v){m.push(v[0])});
+				frm.set_df_property("ashbee_attribute_value", "options", m.join("\n"), child.name, "items");
+				refresh_field("ashbee_attribute_value",child.name, "items");
+			}
+		});
+	}	
+}
+
+var set_ashbee_finished_item = function(frm, cdt, cdn){
+	var child = locals[cdt][cdn];
+	child.ashbee_finished_item = "";
+	refresh_field("ashbee_finished_item", child.name, "items");
+	if(child.ashbee_recipient_task != ""){
+		var _args = {
+					'item_code':child.item_code,
+					'attr_value':child.ashbee_attribute_value,
+					'attr_type':child.ashbee_attribute_type
+				}
+		frappe.call({
+			method:"ashbee.ashbee.customs.stock_entry.get_finished_variant_item",
+			args:_args,
+			callback:function(r){
+				if(r.message){
+					child.ashbee_finished_item = r.message.name;
+					child.ashbee_finished_item_valuation = parseFloat(r.message.rate);
+					
+					refresh_field("ashbee_finished_item", child.name, "items");
+					refresh_field("ashbee_finished_item_valuation", child.name, "items");
+				}
+			}
+		});
+	}
+}
+
+var ash_create_variant = function(frm, cdt, cdn){
+	var child = locals[cdt][cdn];
+	var d = new frappe.ui.Dialog({
+		title: "Create Variant",
+		fields: [
+			{fieldname: "valuation_rate", label: __('Valuation Rate'), fieldtype:"Currency", default:0.0},
+		],
+		primary_action_label: __('Create Variant'),
+		primary_action: () => {
+			d.get_primary_btn().attr('disabled', true);
+			var _args = {
+							'item_code':child.item_code,
+							'valuation_rate':d.get_values().valuation_rate,
+							'attr_value':child.ashbee_attribute_value,
+							'attr_type':child.ashbee_attribute_type
+						}
+			if(_args) {
+				frappe.call({
+					method:"ashbee.ashbee.customs.stock_entry.create_variant_item",
+					args:_args,
+					callback:function(r){
+						if(r.message){
+							d.hide();
+							frappe.show_alert("Items Updated!")
+							child.ashbee_attribute_value = "";
+							refresh_field("ashbee_attribute_value", child.name, "items");
+
+						}
+						
+					}
+				});
+			}
+		}
+	});
+	d.show();
+}
+
+var set_page_primary_action = function(frm){
+		var data = {};
+		$.each(frm.doc.items, (i, v)=>{
+			if(v.ashbee_recipient_task != "" && v.ashbee_attribute_type != "" && v.ashbee_attribute_value != ""
+				&& v.ashbee_finished_item == "" && v.item_code != ""){
+
+				frm.page.set_primary_action(__("Save"), function() {
+			   		frappe.confirm(__("Create Variants and Save?"), function(){
+			   			create_variants_and_save(frm);
+			   			return false;
+
+			   		});
+		       });
+			}
+		});
+
+}
+
+var create_variants_and_save = function(frm){
+	var attrs = [];
+	$.each(frm.doc.items, (i, v)=>{
+		if(v.ashbee_recipient_task != "" && v.ashbee_attribute_type != "" && v.ashbee_attribute_value != ""
+				&& v.ashbee_finished_item == "" && v.item_code != ""){
+			attrs.push({
+						'cdn':v.name,
+						'item_code':v.item_code, 
+						'attr_type':v.ashbee_attribute_type, 
+						'attr_value':v.ashbee_attribute_value
+					});
+		}
+		if((i+1) >= frm.doc.items.length && attrs.length > 0){
+			frappe.call({
+				method:"ashbee.ashbee.customs.stock_entry.create_multiple_variants",
+				args:attrs,
+				callback:function(r){
+					if(r.message){
+						$(r.message).each(function(i){
+							var child = locals[v.doctype][this.cdn];
+							child.ashbee_finished_item = this.variant.name;
+							child.ashbee_finished_item_valuation = this.variant.valuation_rate
+							refresh_field("ashbee_finished_item_valuation", child.name, "items");
+							refresh_field("ashbee_finished_item", child.name, "items");
+							if((i+1) >= r.message.length){
+								frappe.show_alert("Items Updated!");
+								frm.save();
+								window.location.reload();
+							}
+
+						});
+
+					}
+				}
+			});
+		}
+		
+	});
+}
+
+var refresh_all_child_fields = function(frm){
+	$.each(frm.doc.items, (i, v)=>{
+		ashbee_attribute_values_populate(frm, v.doctype, v.name);
+		set_ashbee_finished_item(frm, v.doctype, v.name);
+	});
+}
+
+var empty_child_fields = function(frm, cdt, cdn){
+	var child = locals[cdt][cdn];
+	child.ashbee_finished_item = "";
+	child.ashbee_attribute_type = "";
+	child.ashbee_attribute_value = "";
+	child.ashbee_finished_item_valuation = "";
+
+	refresh_field("ashbee_finished_item_valuation", child.name, "items");
+	refresh_field("ashbee_finished_item", child.name, "items");
+	refresh_field("ashbee_attribute_type", child.name, "items");
+	refresh_field("ashbee_attribute_value", child.name, "items");
+}
+
+frappe.ui.form.on('Stock Entry Detail',{
+
+	ashbee_attribute_type:function(frm, cdt, cdn){
+		ashbee_attribute_values_populate(frm, cdt, cdn);
+	},
+	ashbee_attribute_value:function(frm, cdt, cdn){
+		set_ashbee_finished_item(frm, cdt, cdn);
+	},
+	ashbee_recipient_task:function(frm, cdt, cdn){
+		empty_child_fields(frm, cdt, cdn);
+	},
+
+	item_code:function(frm, cdt, cdn){
+		empty_child_fields(frm, cdt, cdn);
+	},
+
+	ashbee_create_variant:function(frm, cdt, cdn){
+		ash_create_variant(frm, cdt, cdn);
+
+	},
+
+});
 
 frappe.ui.form.on('Stock Entry',{
 
@@ -15,17 +207,8 @@ frappe.ui.form.on('Stock Entry',{
 	},
 
 	refresh:function(frm){
-		frm.set_query("ashbee_finished_item","items", function(doc,cdt,cdn) {
-			var child = locals[cdt][cdn];
-				return {
-					query:"ashbee.ashbee.customs.stock_entry.get_variant_items",
-					filters: {
-						'item_code':child.item_code,
-						'attr_type':child.ashbee_attribute_type
-
-					}
-				};
-			});
+		set_page_primary_action(frm);
+		
 	},
 
 	ashbee_issue_items:function(frm){
