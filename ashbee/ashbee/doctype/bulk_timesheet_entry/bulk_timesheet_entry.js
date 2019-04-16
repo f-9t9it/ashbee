10fb7a115ab7e834dabcd1499d635922228cad19
Brown-Harry Boma <brownharryb@gmail.com>
2019-04-16 23:18:07 +0100

    Update Bulk Timesheet Entry

--- ashbee/ashbee/doctype/bulk_timesheet_entry/bulk_timesheet_entry.js
+++ ashbee/ashbee/doctype/bulk_timesheet_entry/bulk_timesheet_entry.js
@@ -1,32 +1,129 @@
 // Copyright (c) 2019, 9t9IT and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Bulk Timesheet Entry', {
-	refresh: function(frm) {
-		if(frm.doc.start_date_time == null){
-			frm.doc.start_date_time = frappe.datetime.get_today();
-			frm.refresh_field("start_date_time");
+
+var set_start_date_time = function(frm, cdt, cdn){
+	let child = locals[cdt][cdn];
+
+	if(!child.start_date_time) {
+		// if from_time value is not available then set the current datetime
+		frappe.model.set_value(cdt, cdn, "start_date_time", frappe.datetime.get_datetime_as_string());
+	}
+}
+
+var calculate_end_date_time = function(frm, cdt, cdn){
+	let child = locals[cdt][cdn];
+	let d = moment(child.start_date_time);
+	var time_diff = (moment(child.end_time).diff(moment(child.start_date_time),"seconds")) / (60 * 60 * 24);
+		var std_working_hours = 0;
+		var hours = moment(child.end_date_time).diff(moment(child.end_date_time), "seconds") / 3600;
+
+		std_working_hours = time_diff * frappe.working_hours;
+
+		if (std_working_hours < hours && std_working_hours > 0) {
+			frappe.model.set_value(cdt, cdn, "normal_hours", std_working_hours);
+			frappe.model.set_value(cdt, cdn, "end_date_time", d.add(hours, "normal_hours").format(frappe.defaultDatetimeFormat));
+		} else {
+			d.add(child.normal_hours, "hours");
+			frm._setting_hours = true;
+			frappe.model.set_value(cdt, cdn, "end_date_time",
+				d.format(frappe.defaultDatetimeFormat)).then(() => {
+					frm._setting_hours = false;
+				});
+		}
+}
+
+
+var calculate_end_time = function(frm, cdt, cdn) {
+
+	let child = locals[cdt][cdn];
+
+	set_start_date_time (frm, cdt, cdn);
+	if(child.normal_hours) {
+		calculate_end_date_time(frm, cdt, cdn);
+	}
+}
+
+var calculate_ots = function(frm, cdt, cdn){
+	let child = locals[cdt][cdn];
+
+	child.ot1 = child.normal_hours * child.hourly_cost * 1.25;
+	child.ot2 = child.normal_hours * child.hourly_cost * 1.50;
+
+	refresh_field("ot1", child.name, "details");
+	refresh_field("ot2", child.name, "details");
+}
+
+var calculate_total_cost = function(frm, cdt, cdn){
+	var child = locals[cdt][cdn];
+	var normal_hours = child.normal_hours == undefined ? 0 : child.normal_hours; 
+	var hourly_cost = child.hourly_cost == undefined ? 0 : child.hourly_cost;
+	calculate_ots(frm, cdt, cdn);
+	child.total_cost = (normal_hours * hourly_cost) + child.ot1 + child.ot2;
+	refresh_field("total_cost", child.name, "details");
+
+}
+
+
+frappe.ui.form.on('Bulk Timesheet Details', {
+
+	start_date_time:function(frm, cdt, cdn){
+		// calculate_hrs(frm, cdt, cdn);
+		calculate_end_time(frm, cdt, cdn);
+	},
+
+	end_date_time:function(frm, cdt, cdn){
+		var child = locals[cdt][cdn];
+		var time_diff = (moment(child.end_date_time).diff(moment(child.start_date_time),"seconds")) / ( 60 * 60 * 24);
+		var std_working_hours = 0;
+
+		if(frm._setting_hours) return;
+
+		var hours = moment(child.end_date_time).diff(moment(child.start_date_time), "seconds") / 3600;
+		std_working_hours = time_diff * frappe.working_hours;
+
+		if (std_working_hours < hours && std_working_hours > 0) {
+			frappe.model.set_value(cdt, cdn, "normal_hours", std_working_hours);
+		} else {
+			frappe.model.set_value(cdt, cdn, "normal_hours", hours);
 		}
 	},
 
-	employee:function(frm){
+	normal_hours:function(frm, cdt, cdn){
+		calculate_end_time(frm, cdt, cdn);
+		calculate_total_cost(frm, cdt, cdn);
+
+	},
+
+	employee:function(frm, cdt, cdn){
+		var child = locals[cdt][cdn];
 		frm.call({
-			method:"get_employee_name",
+			method:"get_employee_details",
+			args:{"employee":child.employee},
 			doc:frm.doc,
 			callback:function(r){
-				frm.doc.employee_name = r.message;
-				frm.refresh_field("employee_name");
+				child.employee_name = r.message.name;
+				child.hourly_cost = r.message.rate;
+				refresh_field("employee_name",child.name, "details");
+				refresh_field("hourly_cost",child.name, "details");
 			}
 		});
 	},
 
-	project:function(frm){
+	hourly_cost:function(frm, cdt, cdn){
+		calculate_total_cost(frm, cdt, cdn);
+	},
+
+
+	project:function(frm, cdt, cdn){
+		var child = locals[cdt][cdn];
 		frm.call({
 			method:"get_project_name",
+			args:{'project':child.project},
 			doc:frm.doc,
 			callback:function(r){
-				frm.doc.project_name = r.message;
-				frm.refresh_field("project_name")
+				child.project_name = r.message;
+				refresh_field("project_name",child.name, "details");
 			}
 		});
 	}
--- ashbee/ashbee/doctype/bulk_timesheet_entry/bulk_timesheet_entry.py
+++ ashbee/ashbee/doctype/bulk_timesheet_entry/bulk_timesheet_entry.py
@@ -5,14 +5,70 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import today
+from erpnext.hr.doctype.salary_structure_assignment.salary_structure_assignment import get_assigned_salary_structure
 
 class BulkTimesheetEntry(Document):
 	
 
-	def get_employee_name(self):
-		employee = frappe.get_doc("Employee", self.employee)
-		return employee.employee_name
+	def get_employee_details(self, employee):
+		employee = frappe.get_doc("Employee", employee)
+		return {
+					"name":employee.employee_name, 
+					"rate":self.get_employee_charge_per_hour(employee)
+				}
 
-	def get_project_name(self):
-		project = frappe.get_doc("Project",self.project)
+	def get_project_name(self, project):
+		project = frappe.get_doc("Project",project)
 		return project.name
+
+	def get_employee_charge_per_hour(self, employee):
+		salary_structure = get_assigned_salary_structure(employee.name, today())
+		if not salary_structure:
+			return 0.0
+		salary_structure = frappe.get_doc("Salary Structure", salary_structure)
+		if salary_structure.docstatus != 1 or not salary_structure.is_active == "Yes" or \
+						not salary_structure.salary_slip_based_on_timesheet:
+			return 0.0
+		return salary_structure.hour_rate
+
+	def validate(self):
+		self.update_timesheet()
+
+
+	def update_timesheet(self):
+		for detail in self.details:
+			timesheet = detail.timesheet
+			if not timesheet:
+				timesheet = frappe.new_doc("Timesheet")
+			else:
+				timesheet = frappe.get_doc("Timesheet", timesheet)
+			timesheet.company = self.company
+			timesheet.employee = detail.employee
+			timesheet.time_logs = self.get_timesheet_timelogs(timesheet, detail)
+			timesheet.save(ignore_permissions=True)
+			timesheet.submit()
+			detail.timesheet = timesheet.name
+
+
+
+	def get_timesheet_timelogs(self, timesheet, entry_detail):
+		timesheet_details = []
+		if not hasattr(timesheet, "time_logs") or not timesheet.time_logs:
+			detail = frappe.new_doc("Timesheet Detail")
+		else:
+			detail = timesheet.time_logs[0]
+			
+
+		detail.parentfield = "time_logs"
+		detail.activity_type = entry_detail.activity_type
+		detail.hours = entry_detail.normal_hours
+		detail.from_time = entry_detail.start_date_time
+		detail.to_time = entry_detail.end_date_time
+		detail.billable = True
+		detail.costing_rate = entry_detail.hourly_cost
+		detail.costing_amount = entry_detail.total_cost
+		detail.project = entry_detail.project
+
+		timesheet_details.append(detail)
+		return timesheet_details
