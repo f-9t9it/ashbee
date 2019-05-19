5958e0bb3de01e6dfbab68f711815b3ff08444bc
Ivan Ray Altomera <irayspacii@gmail.com>
2019-05-19 16:04:17 +0800

    fix: ashbee_bar checked if variant of derived is bar checked

--- ashbee/ashbee/customs/items.py
+++ ashbee/ashbee/customs/items.py
@@ -1,5 +1,9 @@
+import frappe
 
 
 def item_save(doc, d):
+	if doc.variant_of:
+		ashbee_bar = frappe.db.get_value('Item', doc.variant_of, 'ashbee_bar')
+		doc.ashbee_bar = ashbee_bar
 	if doc.ashbee_bar:
 		doc.weight_per_unit = doc.ashbee_weight
