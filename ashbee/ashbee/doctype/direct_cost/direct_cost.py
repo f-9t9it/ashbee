ea87c5bd55378a4b76517e711355286de4dc3b64
Ivan Ray Altomera <irayspacii@gmail.com>
2019-05-21 17:21:34 +0800

    feat: update total_direct_cost for Project from Direct Cost activities

--- ashbee/ashbee/doctype/direct_cost/direct_cost.py
+++ ashbee/ashbee/doctype/direct_cost/direct_cost.py
@@ -6,5 +6,26 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class DirectCost(Document):
-	pass
+	def on_submit(self):
+		for item in self.items:
+			total_direct_cost = _get_total_direct_cost(item.job_no)
+			added_direct_cost = total_direct_cost + item.direct_cost
+			_set_total_direct_cost(item.job_no, added_direct_cost)
+		frappe.db.commit()
+
+	def on_cancel(self):
+		for item in self.items:
+			total_direct_cost = _get_total_direct_cost(item.job_no)
+			subtracted_direct_cost = total_direct_cost - item.direct_cost
+			_set_total_direct_cost(item.job_no, subtracted_direct_cost)
+		frappe.db.commit()
+
+
+def _get_total_direct_cost(job_no):
+	return frappe.db.get_value('Project', job_no, 'ashbee_total_direct_cost')
+
+
+def _set_total_direct_cost(job_no, direct_cost):
+	frappe.db.set_value('Project', job_no, 'ashbee_total_direct_cost', direct_cost)
