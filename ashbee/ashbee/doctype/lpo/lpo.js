cbe5a8a544b0e69b5a0d566493c3ba7e573a6ab8
Ivan Ray Altomera <irayspacii@gmail.com>
2019-05-22 21:04:29 +0800

    feat: added logic in total amount and qty

--- ashbee/ashbee/doctype/lpo/lpo.js
+++ ashbee/ashbee/doctype/lpo/lpo.js
@@ -6,3 +6,47 @@ frappe.ui.form.on('LPO', {
 
 	}
 });
+
+frappe.ui.form.on('LPO Item', {
+	rate: function(frm, cdt, cdn) {
+		_update_amount(frm, cdt, cdn);
+		_update_total_amount(frm);
+		_update_total_qty(frm);
+		frm.refresh();
+	},
+	qty: function(frm, cdt, cdn) {
+		_update_amount(frm, cdt, cdn);
+		_update_total_amount(frm);
+		_update_total_qty(frm);
+		frm.refresh();
+	}
+});
+
+var _update_amount = function(frm, cdt, cdn) {
+	var item = locals[cdt][cdn];
+	frappe.model.set_value(cdt, cdn, 'amount', item.rate * item.qty);
+	frm.refresh_field('items');
+};
+
+var _update_total_amount = function(frm) {
+	var total = 0.00;
+
+	$.each(frm.doc.items, function(i, v) {
+		total = total + v.amount;
+	});
+
+	frm.set_value('total', total);
+	frm.set_value('net_total', total);
+	frm.set_value('base_total', total);
+	frm.set_value('base_net_total', total);
+};
+
+var _update_total_qty = function(frm) {
+	var total = 0;
+
+	$.each(frm.doc.items, function(i, v) {
+		total = total + v.qty;
+	});
+
+	frm.set_value('total_qty', total);
+}
