8ab110018cdb6353fe4c4a82736b1273016a6c54
Ivan Ray Altomera <irayspacii@gmail.com>
2019-05-26 16:23:30 +0800

    feat: make issue transition from Material Request to Stock Entry

--- ashbee/public/js/material_request.js
+++ ashbee/public/js/material_request.js
@@ -1,3 +1,9 @@
+frappe.ui.form.on('Material Request', {
+    refresh: function(frm) {
+        _make_custom_button(frm);
+    }
+});
+
 frappe.ui.form.on('Material Request Item', {
     items_add: function(frm, cdt, cdn) {
         frappe.model.set_value(cdt, cdn, 'project', frm.doc.project);
@@ -11,6 +17,33 @@ frappe.ui.form.on('Material Request Item', {
         ashbee_attribute_values_populate(frm, cdt, cdn);
     }
 });
+
+var _make_custom_button = function(frm) {
+    frm.add_custom_button(__('Make Issue'), function() {
+        frappe.model.with_doctype('Stock Entry', function() {
+            var se = frappe.model.get_new_doc('Stock Entry');
+            se.purpose = 'Material Issue';
+
+            var items = frm.doc.items;
+            items.forEach(function(item) {
+                var se_item = frappe.model.add_child(se, 'items');
+                    se_item.item_code = item.item_code;
+                    se_item.basic_rate = item.rate;
+                    se_item.item_name = item.item_name;
+                    se_item.uom = item.uom;
+                    se_item.conversion_factor = item.conversion_factor;
+                    se_item.item_group = item.item_group;
+                    se_item.description = item.description;
+                    se_item.image = item.image;
+                    se_item.qty = item.qty;
+                    se_item.warehouse = item.warehouse;
+                    se_item.required_date = frappe.datetime.nowdate();
+            });
+
+            frappe.set_route('Form', 'Stock Entry', se.name);
+        });
+    })
+};
 
 var ashbee_attribute_values_populate = function(frm, cdt, cdn) {
     var child = locals[cdt][cdn];
