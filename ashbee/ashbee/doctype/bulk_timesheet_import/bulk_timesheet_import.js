4a52c5f34d9235096329f371eee51d20532b26f1
Ivan Ray Altomera <irayspacii@gmail.com>
2019-05-26 15:45:22 +0800

    feat: added Bulk Timesheet Import doctype

---
+++ ashbee/ashbee/doctype/bulk_timesheet_import/bulk_timesheet_import.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, 9t9IT and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bulk Timesheet Import', {
+	refresh: function(frm) {
+
+	}
+});
