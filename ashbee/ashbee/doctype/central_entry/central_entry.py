5799d3d78db940013c1aefe76fe4620bc92635ab
Ivan Ray Altomera <irayspacii@gmail.com>
2019-05-29 12:18:44 +0800

    feat: set project costing under Central Entry actions

--- ashbee/ashbee/doctype/central_entry/central_entry.py
+++ ashbee/ashbee/doctype/central_entry/central_entry.py
@@ -6,5 +6,40 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class CentralEntry(Document):
-	pass
+	def on_submit(self):
+		_set_project_costing(self.items, self.voucher_type)
+
+	def on_cancel(self):
+		_set_project_costing(self.items, self.voucher_type, cancel=True)
+
+
+def _set_project_costing(items, voucher_type, cancel=False):
+	for item in items:
+		value = _get_project_central_field_value(item.project, voucher_type)
+		allocated = item.allocated if not cancel else -item.allocated
+		_set_project_central_field_value(
+			item.project,
+			voucher_type,
+			value + allocated
+		)
+	frappe.db.commit()
+
+
+def _get_project_central_field_value(project, voucher_type):
+	fields = {
+		'Timesheet': 'ashbee_total_central_labor',
+		'Stock Entry': 'ashbee_total_central_cost'
+	}
+
+	return frappe.db.get_value('Project', project, fields[voucher_type])
+
+
+def _set_project_central_field_value(project, voucher_type, value):
+	fields = {
+		'Timesheet': 'ashbee_total_central_labor',
+		'Stock Entry': 'ashbee_total_central_cost'
+	}
+
+	frappe.db.set_value('Project', project, fields[voucher_type], value)
