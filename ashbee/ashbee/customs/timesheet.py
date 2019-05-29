6d68f3f5d39224d6b6edc70fb349fafbd02cc23d
Ivan Ray Altomera <irayspacii@gmail.com>
2019-05-29 15:11:00 +0800

    feat: create central entry if it is a central project

--- ashbee/ashbee/customs/timesheet.py
+++ ashbee/ashbee/customs/timesheet.py
@@ -3,27 +3,32 @@ from frappe.utils import flt
 
 
 def timesheet_save(doc, d):
-	for detail in doc.time_logs:
-		if not detail.billable:
-			continue
-		detail.ashbee_ot = flt(detail.ashbee_ot) or 0.0
-		detail.ashbee_ot2 = flt(detail.ashbee_ot2) or 0.0
-		detail.costing_amount = flt(detail.costing_amount) or 0.0
-		doc.total_costing_amount = flt(doc.total_costing_amount) or 0.0
+    for detail in doc.time_logs:
+        if not detail.billable:
+            continue
+        detail.ashbee_ot = flt(detail.ashbee_ot) or 0.0
+        detail.ashbee_ot2 = flt(detail.ashbee_ot2) or 0.0
+        detail.costing_amount = flt(detail.costing_amount) or 0.0
+        doc.total_costing_amount = flt(doc.total_costing_amount) or 0.0
 
-		detail.costing_amount += detail.ashbee_ot + detail.ashbee_ot2
-		doc.total_costing_amount += detail.ashbee_ot + detail.ashbee_ot2
+        detail.costing_amount += detail.ashbee_ot + detail.ashbee_ot2
+        doc.total_costing_amount += detail.ashbee_ot + detail.ashbee_ot2
 
 
 def timesheet_submit(doc, d):
-	for detail in doc.time_logs:
-		central_entry = frappe.get_doc({
-			'doctype': 'Central Entry',
-			'posting_date': doc.start_date,
-			'voucher_type': 'Timesheet',
-			'voucher_no': doc.name,
-			'voucher_detail_no': detail.name,
-			'allocation': detail.costing_amount
-		})
+    central_project = frappe.db.get_single_value('Ashbee Settings', 'central_project')
 
-		central_entry.insert()
+    for detail in doc.time_logs:
+        if detail.project == central_project:
+            central_entry = _create_central_entry(doc, detail)
+
+
+def _create_central_entry(doc, detail):
+    return frappe.get_doc({
+        'doctype': 'Central Entry',
+        'posting_date': doc.start_date,
+        'voucher_type': 'Timesheet',
+        'voucher_no': doc.name,
+        'voucher_detail_no': detail.name,
+        'allocation': detail.costing_amount
+    }).insert()
