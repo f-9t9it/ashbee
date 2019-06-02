66089a9cfa9c4a49bf3d85043c8fd024accf1873
Ivan Ray Altomera <irayspacii@gmail.com>
2019-06-02 17:31:48 +0800

    feat: LPO add grand total

--- ashbee/ashbee/doctype/lpo/lpo.py
+++ ashbee/ashbee/doctype/lpo/lpo.py
@@ -6,5 +6,15 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class LPO(Document):
-	pass
+	def validate(self):
+		self._calculate_totals()
+
+	def _calculate_totals(self):
+		totals = 0.0
+		for item in self.items:
+			totals = totals + (item.qty * item.rate)
+		self.base_grand_total = totals
+		self.grand_total = totals
+
