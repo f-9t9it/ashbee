a77c667a300fefbb6fa9c7b836715ee013f3a51f
Ivan Ray Altomera <irayspacii@gmail.com>
2019-06-02 17:40:25 +0800

    feat: LPO in words grand total

--- ashbee/ashbee/doctype/lpo/lpo.py
+++ ashbee/ashbee/doctype/lpo/lpo.py
@@ -5,11 +5,13 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils.data import money_in_words
 
 
 class LPO(Document):
 	def validate(self):
 		self._calculate_totals()
+		self._set_money_in_words()
 
 	def _calculate_totals(self):
 		totals = 0.0
@@ -18,3 +20,6 @@ class LPO(Document):
 		self.base_grand_total = totals
 		self.grand_total = totals
 
+	def _set_money_in_words(self):
+		self.in_words = money_in_words(self.grand_total, self.currency)
+		self.base_in_words = money_in_words(self.base_grand_total, self.currency)
