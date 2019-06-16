6dcc3b0d147ffdaa3c6606d312d85ff7ea112fc1
Ivan Ray Altomera <irayspacii@gmail.com>
2019-06-16 18:17:09 +0800

    feat: color variant subtitle under Purchase Receipt

--- ashbee/public/js/purchase_receipt.js
+++ ashbee/public/js/purchase_receipt.js
@@ -3,4 +3,18 @@ frappe.ui.form.on('Purchase Receipt', {
         let currency = frm.doc.currency;
         frm.set_value('naming_series', currency === 'BHD' ? 'PRL-.YY.-.#####' : 'PRF-.YY.-.#####');
     }
-})
+});
+
+// override
+frappe.provide('ashbee.stock');
+
+ashbee.stock.PurchaseReceiptController = erpnext.stock.PurchaseReceiptController.extend({
+    onload: function(frm) {
+        this._super();
+        this.frm.set_query('item_code', 'items', function() {
+            return { query: 'ashbee.queries.item_query' };
+        });
+    }
+});
+
+$.extend(cur_frm.cscript, new ashbee.stock.PurchaseReceiptController({ frm: cur_frm }));
