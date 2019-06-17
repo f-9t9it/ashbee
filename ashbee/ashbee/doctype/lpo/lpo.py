0b87cd6d2f924b72a19f5d63ded004bdbdc8c42c
Ivan Ray Altomera <irayspacii@gmail.com>
2019-06-17 22:36:02 +0800

    fix: update taxes computation

--- ashbee/ashbee/doctype/lpo/lpo.py
+++ ashbee/ashbee/doctype/lpo/lpo.py
@@ -34,6 +34,7 @@ class LPO(Document):
 		taxes_and_charges_deducted = 0.00
 
 		for tax in self.taxes:
+			self._calculate_taxes(tax)
 			if tax.add_deduct_tax == "Add":
 				taxes_and_charges_added = taxes_and_charges_added + tax.tax_amount
 			else:
@@ -48,32 +49,29 @@ class LPO(Document):
 		self.total_taxes_and_charges = self.base_total_taxes_and_charges
 
 	def _calculate_totals(self):
-		totals = 0.0
-
-		for item in self.items:
-			totals = totals + (item.qty * item.rate)
-
-		self.base_grand_total = totals + self.base_total_taxes_and_charges
-		self.grand_total = totals + self.total_taxes_and_charges
+		self.base_grand_total = self.base_net_total + self.base_total_taxes_and_charges
+		self.grand_total = self.net_total + self.total_taxes_and_charges
 
 	def _apply_discount(self):
-		if not self.additional_discount_percentage:
-			return
-
-		total_amount = self.grand_total
-
+		self._calculate_discount()
 		if self.apply_discount_on == 'Net Total':
-			total_amount = self.net_total
-
-		discount_amount = total_amount / self.additional_discount_percentage
-		self.base_discount_amount = discount_amount
-		self.discount_amount = discount_amount
-
-		if self.apply_discount_on == 'Net Total':
-			self.net_total = self.net_total - discount_amount
+			self.net_total = self.total - self.discount_amount
+			self.base_net_total = self.base_total - self.discount_amount
 		else:
-			self.grand_total = self.grand_total - discount_amount
+			self.grand_total = self.net_total - self.discount_amount
+			self.base_grand_total = self.base_net_total - self.discount_amount
 
 	def _set_money_in_words(self):
 		self.in_words = money_in_words(self.grand_total, self.currency)
 		self.base_in_words = money_in_words(self.base_grand_total, self.currency)
+
+	def _calculate_taxes(self, tax):
+		if tax.rate:
+			tax.tax_amount = self.net_total * (tax.rate / 100.00)
+
+	def _calculate_discount(self):
+		if not self.discount_amount:
+			self.discount_amount = 0.00
+
+		if self.additional_discount_percentage:
+			self.discount_amount = self.total * (self.additional_discount_percentage / 100.00)
