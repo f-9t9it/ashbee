ca726830a3e395dc43d4032680f03943d6fa3c00
Ivan Ray Altomera <irayspacii@gmail.com>
2019-06-20 15:44:00 +0800

    feat: group allocation by cost and labor

--- ashbee/ashbee/doctype/central_expense/central_expense.py
+++ ashbee/ashbee/doctype/central_expense/central_expense.py
@@ -5,7 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from ashbee.utils import get_month_date_range
+from ashbee.utils import get_costs_by_projects, get_month_date_range
 
 from toolz import partial
 
@@ -16,13 +16,35 @@ class CentralExpense(Document):
 		self.projects = []
 
 		date_range = get_month_date_range(self.posting_date)
+
 		central_entries = _get_central_entries(date_range)
 		self._set_central_entries(central_entries)
 
+		projects = get_costs_by_projects(date_range)
+		self._set_projects(projects)
+
 	def _set_central_entries(self, central_entries):
-		append_and_sum = partial(_append_and_sum, self)
-		total_allocation = reduce(append_and_sum, central_entries, 0.00)
-		self.total_allocation = total_allocation
+		append_and_sum = partial(_append_and_sum_central_entry, self)
+		allocation = reduce(append_and_sum, central_entries, {
+			'labor_allocation': 0.00,
+			'cost_allocation': 0.00
+		})
+		self.labor_allocation = allocation['labor_allocation']
+		self.cost_allocation = allocation['cost_allocation']
+		self.total_allocation = self.labor_allocation + self.cost_allocation
+
+	def _set_projects(self, projects):
+		total_cost = reduce(lambda x, y: x + y, projects.values())
+		for project, cost in projects.iteritems():
+			ratio = cost / total_cost
+			allocation = self.total_allocation * ratio
+			self.append('projects', {
+				'project': project,
+				'cost': cost,
+				'ratio': ratio,
+				'allocation': allocation
+			})
+		self.total_cost = total_cost
 
 
 def _get_central_entries(date_range):
@@ -33,14 +55,25 @@ def _get_central_entries(date_range):
 	""", date_range, as_dict=1)
 
 
-def _append_and_sum(central_expense, x, y):
+def _append_and_sum_central_entry(central_expense, x, y):
 	"""
 	Use central expense to append to the table,
 	and summation of x and y (used for reduce)
 	:param central_expense:
-	:param x: total_allocation
+	:param x: allocation; labor and cost
 	:param y: Central Entry
-	:return: total_allocation
+	:return: allocation
 	"""
 	central_expense.append('items', y)
-	return x + y['allocation']
+
+	allocation = y['allocation']
+	entry_type = y['entry_type']
+
+	if entry_type == 'Stock Entry':
+		labor_allocation = x['labor_allocation'] + allocation
+		x['labor_allocation'] = labor_allocation
+	elif entry_type == 'Timesheet':
+		cost_allocation = x['cost_allocation'] + allocation
+		x['cost_allocation'] = cost_allocation
+
+	return x
