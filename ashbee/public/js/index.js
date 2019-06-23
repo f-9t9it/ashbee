933c83e0e7edaf8e15f2f0e0c68a1bb8d44b0ca9
Ivan Ray Altomera <irayspacii@gmail.com>
2019-06-23 19:10:40 +0800

    feat: batch quick entry

---
+++ ashbee/public/js/index.js
@@ -0,0 +1,3 @@
+import { batch_qe } from './scripts';
+
+frappe.ui.form.BatchQuickEntryForm = frappe.ui.form.QuickEntryForm.extend(batch_qe);
---
+++ ashbee/public/js/scripts/index.js
@@ -0,0 +1,1 @@
+export { default as batch_qe } from './batch_qe';
