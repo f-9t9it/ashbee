33a06dda1f1e0bfb0acdc19ba5bd3073cbfc9c99
Ivan Ray Altomera <irayspacii@gmail.com>
2019-06-23 22:01:50 +0800

    feat: View > Stock Ledger in PR redirect to Ashbee Stock Movement

--- ashbee/public/js/purchase_receipt.js
+++ ashbee/public/js/purchase_receipt.js
@@ -14,6 +14,19 @@ ashbee.stock.PurchaseReceiptController = erpnext.stock.PurchaseReceiptController
         this.frm.set_query('item_code', 'items', function() {
             return { query: 'ashbee.queries.item_query' };
         });
+    },
+    show_stock_ledger: function(frm) {
+        var me = this;
+		if (this.frm.doc.docstatus === 1) {
+			cur_frm.add_custom_button(__("Stock Ledger"), function() {
+				frappe.route_options = {
+					voucher_no: me.frm.doc.name,
+					from_date: me.frm.doc.posting_date,
+					to_date: me.frm.doc.posting_date
+				};
+				frappe.set_route("query-report", "Ashbee Stock Movement");
+			}, __("View"));
+		}
     }
 });
 
