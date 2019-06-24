3e0ad515861cef7e24814f6ea9007ac65df31405
Ivan Ray Altomera <irayspacii@gmail.com>
2019-06-24 15:55:35 +0800

    added initial columns

--- ashbee/ashbee/report/ashbee_single_job_cost/ashbee_single_job_cost.py
+++ ashbee/ashbee/report/ashbee_single_job_cost/ashbee_single_job_cost.py
@@ -4,6 +4,21 @@
 from __future__ import unicode_literals
 import frappe
 
+from ashbee.helpers import new_column
+
+
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = get_columns(), get_data(filters)
 	return columns, data
+
+
+def get_columns():
+	return [
+		new_column("Reference", "reference", "Data", 95),
+		new_column("Date", "date", "Date", 95),
+		new_column("Description", "description", "Data", 200)
+	]
+
+
+def get_data(filters):
+	return []
