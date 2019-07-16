d55db0183904ba9e123750fbb3f2edb28fc6473a
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-16 17:11:59 +0800

    fix: logic in creation of central entry; feat: generate central entries from Ashbee Settings

--- ashbee/ashbee/doctype/ashbee_settings/ashbee_settings.js
+++ ashbee/ashbee/doctype/ashbee_settings/ashbee_settings.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Ashbee Settings', {
-	refresh: function(frm) {
-
+	create_central_entries: function(frm) {
+		frm.call({
+			doc: frm.doc,
+			method: 'create_central_entries',
+			callback: function(r) {
+				console.log(r);
+			}
+		});
 	}
 });
