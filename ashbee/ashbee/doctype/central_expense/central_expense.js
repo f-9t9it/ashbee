ddc81b012c35776d248b2fec3d4065e93713b589
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-21 00:01:07 +0800

    fix: removed read_only for posting date

--- ashbee/ashbee/doctype/central_expense/central_expense.js
+++ ashbee/ashbee/doctype/central_expense/central_expense.js
@@ -9,7 +9,6 @@ frappe.ui.form.on('Central Expense', {
 
 
 var _set_posting_date = function(frm) {
-	frm.set_df_property('posting_date', 'read_only', 1);
 	if (frm.doc.__islocal) {
 		frm.set_value('posting_date', frappe.datetime.now_date());
 	}
