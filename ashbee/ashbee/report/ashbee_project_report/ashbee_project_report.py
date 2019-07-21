d49382019329441ebbc2ff147728f9a808eab29d
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-21 22:40:28 +0800

    fix: set default for central_labour and central_expenses; avoid NoneType

--- ashbee/ashbee/report/ashbee_project_report/ashbee_project_report.py
+++ ashbee/ashbee/report/ashbee_project_report/ashbee_project_report.py
@@ -176,6 +176,10 @@ def _group_centrals(data):
 def _fill_central_costs(data, central_costs):
     for row in data:
         project = row.get('project')
+
+        row['central_labour'] = 0.00
+        row['central_expenses'] = 0.00
+
         if central_costs.get(project, None):
             row['central_labour'] = central_costs[project].get('labor')
             row['central_expenses'] = central_costs[project].get('cost')
