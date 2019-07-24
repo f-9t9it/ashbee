01580e01827637ad62dfe28b6226b23b91a3951c
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-24 14:48:34 +0800

    feat: fill totals in Ashbee Single Job Cost

--- ashbee/ashbee/report/ashbee_single_job_cost/ashbee_single_job_cost.py
+++ ashbee/ashbee/report/ashbee_single_job_cost/ashbee_single_job_cost.py
@@ -8,91 +8,108 @@ from ashbee.helpers import new_column
 
 
 def execute(filters=None):
-	columns, data = get_columns(), get_data(filters)
-	return columns, data
+    columns, data = get_columns(), get_data(filters)
+
+    if data:
+        _fill_totals(data)
+
+    return columns, data
 
 
 def get_columns():
-	return [
-		new_column("Reference", "reference", "Data", 150),
-		new_column("Date", "date", "Date", 95),
-		new_column("Description", "description", "Data", 200),
-		new_column("Income", "income", "Currency", 90),
-		new_column("Qty", "qty", "Int", 90),
-		new_column("Rate", "rate", "Currency", 90),
-		new_column("Material+Direct", "material_direct", "Currency", 120),
-		new_column("Labor Expenses", "labor_expenses", "Currency", 120),
-		new_column("Central Labour", "central_labour", "Currency", 120),
-		new_column("Central Expenses", "central_expenses", "Currency", 120),
-		new_column("Indirect", "indirect", "Currency", 120)
-	]
+    return [
+        new_column("Reference", "reference", "Data", 150),
+        new_column("Date", "date", "Date", 95),
+        new_column("Description", "description", "Data", 200),
+        new_column("Income", "income", "Currency", 90),
+        new_column("Qty", "qty", "Int", 90),
+        new_column("Rate", "rate", "Currency", 90),
+        new_column("Material+Direct", "material_direct", "Currency", 120),
+        new_column("Labor Expenses", "labor_expenses", "Currency", 120),
+        new_column("Central Labour", "central_labour", "Currency", 120),
+        new_column("Central Expenses", "central_expenses", "Currency", 120),
+        new_column("Indirect", "indirect", "Currency", 120)
+    ]
 
 
 def get_data(filters):
-	data = []
+    data = []
 
-	# TODO: know if project_expenses is by date range filters(?)
-	project_expenses = _get_project_expenses(filters)
-	data.append({
-		'material_direct': project_expenses.get('material') + project_expenses.get('direct'),
-		'labor_expenses': project_expenses.get('labor'),
-		'central_labour': project_expenses.get('central_labour'),
-		'central_expenses': project_expenses.get('central_expenses'),
-		'indirect': project_expenses.get('indirect_cost')
-	})
+    # TODO: know if project_expenses is by date range filters(?)
+    project_expenses = _get_project_expenses(filters)
+    data.append({
+        'material_direct': project_expenses.get('material') + project_expenses.get('direct'),
+        'labor_expenses': project_expenses.get('labor'),
+        'central_labour': project_expenses.get('central_labour'),
+        'central_expenses': project_expenses.get('central_expenses'),
+        'indirect': project_expenses.get('indirect_cost')
+    })
 
-	entries = [
-		_get_stock_ledger_entries(filters),
-		_get_timesheet_details(filters)
-	]
+    entries = [
+        _get_stock_ledger_entries(filters),
+        _get_timesheet_details(filters)
+    ]
 
-	for entry in entries:
-		data.extend(entry)
+    for entry in entries:
+        data.extend(entry)
 
-	return data
+    return data
 
 
 def _get_project_expenses(filters):
-	return frappe.db.sql("""
-		SELECT 
-			total_costing_amount AS labor,
-			(ashbee_total_direct_cost + total_purchase_cost) AS direct,
-			total_consumed_material_cost AS material,
-			ashbee_total_central_cost AS central_expenses,
-			ashbee_total_central_labor AS central_labour,
-			ashbee_total_indirect_cost AS indirect_cost
-		FROM `tabProject`
-		WHERE name=%(project)s
-	""", filters, as_dict=1)[0]
+    return frappe.db.sql("""
+        SELECT 
+            total_costing_amount AS labor,
+            (ashbee_total_direct_cost + total_purchase_cost) AS direct,
+            total_consumed_material_cost AS material,
+            ashbee_total_central_cost AS central_expenses,
+            ashbee_total_central_labor AS central_labour,
+            ashbee_total_indirect_cost AS indirect_cost
+        FROM `tabProject`
+        WHERE name=%(project)s
+    """, filters, as_dict=1)[0]
 
 
 def _get_stock_ledger_entries(filters):
-	return frappe.db.sql("""
-		SELECT 
-			posting_date AS date, 
-			voucher_no AS reference, 
-			item_code AS description,
-			valuation_rate AS rate,
-			actual_qty AS qty
-		FROM `tabStock Ledger Entry`
-		WHERE project=%(project)s 
-		AND posting_date BETWEEN %(from_date)s AND %(to_date)s
-	""", filters, as_dict=1)
+    return frappe.db.sql("""
+        SELECT 
+            posting_date AS date, 
+            voucher_no AS reference, 
+            item_code AS description,
+            valuation_rate AS rate,
+            actual_qty AS qty
+        FROM `tabStock Ledger Entry`
+        WHERE project=%(project)s 
+        AND posting_date BETWEEN %(from_date)s AND %(to_date)s
+    """, filters, as_dict=1)
 
 
 def _get_timesheet_details(filters):
-	timesheet_details = frappe.db.sql("""
-		SELECT sum(costing_amount) AS rate, count(*) AS qty
-		FROM `tabTimesheet Detail`
-		INNER JOIN `tabTimesheet`
-		ON `tabTimesheet Detail`.parent = `tabTimesheet`.name
-		WHERE `tabTimesheet Detail`.project=%(project)s
-		AND `tabTimesheet Detail`.docstatus=1
-		AND start_date BETWEEN %(from_date)s AND %(to_date)s
-	""", filters, as_dict=1)
+    timesheet_details = frappe.db.sql("""
+        SELECT sum(costing_amount) AS rate, count(*) AS qty
+        FROM `tabTimesheet Detail`
+        INNER JOIN `tabTimesheet`
+        ON `tabTimesheet Detail`.parent = `tabTimesheet`.name
+        WHERE `tabTimesheet Detail`.project=%(project)s
+        AND `tabTimesheet Detail`.docstatus=1
+        AND start_date BETWEEN %(from_date)s AND %(to_date)s
+    """, filters, as_dict=1)
 
-	if timesheet_details:
-		timesheet_detail = timesheet_details[0]
-		timesheet_detail['reference'] = 'Timesheets'
+    if timesheet_details:
+        timesheet_detail = timesheet_details[0]
+        timesheet_detail['description'] = '<i>Timesheets</i>'
 
-	return timesheet_details
+    return timesheet_details
+
+
+def _fill_totals(data):
+    total_qty = 0
+    total_rate = 0.00
+    for row in data:
+        total_qty = total_qty + row.get('qty', 0)
+        total_rate = total_rate + row.get('rate', 0.00)
+    data.append({
+        'description': '<b>Total</b>',
+        'qty': total_qty,
+        'rate': total_rate
+    })
