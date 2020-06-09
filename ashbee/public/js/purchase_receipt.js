c52ac8c2402b7dbfdccc84273133d7b3a76d3629
Ivan Ray Altomera <irayspacii@gmail.com>
2020-06-09 19:15:33 +0800

    feat(purchase_receipt): project - company filter

--- ashbee/public/js/purchase_receipt.js
+++ ashbee/public/js/purchase_receipt.js
@@ -2,6 +2,13 @@ frappe.ui.form.on('Purchase Receipt', {
     currency: function(frm) {
         let currency = frm.doc.currency;
         frm.set_value('naming_series', currency === 'BHD' ? 'PRL-.YY.-.#####' : 'PRF-.YY.-.#####');
+    },
+    company: function(frm) {
+      frm.set_query("project", "items", function() {
+        return {
+          filters: { company: frm.doc.company }
+        };
+      });
     }
 });
 
@@ -10,23 +17,23 @@ frappe.provide('ashbee.stock');
 
 ashbee.stock.PurchaseReceiptController = erpnext.stock.PurchaseReceiptController.extend({
     onload: function(frm) {
-        this._super();
-        this.frm.set_query('item_code', 'items', function() {
-            return { query: 'ashbee.queries.item_query' };
-        });
+      this._super();
+      this.frm.set_query('item_code', 'items', function() {
+        return { query: 'ashbee.queries.item_query' };
+      });
     },
     show_stock_ledger: function(frm) {
-        var me = this;
-		if (this.frm.doc.docstatus === 1) {
-			cur_frm.add_custom_button(__("Stock Ledger"), function() {
-				frappe.route_options = {
-					voucher_no: me.frm.doc.name,
-					from_date: me.frm.doc.posting_date,
-					to_date: me.frm.doc.posting_date
-				};
-				frappe.set_route("query-report", "Ashbee Stock Movement");
-			}, __("View"));
-		}
+      var me = this;
+      if (this.frm.doc.docstatus === 1) {
+        cur_frm.add_custom_button(__("Stock Ledger"), function() {
+          frappe.route_options = {
+            voucher_no: me.frm.doc.name,
+            from_date: me.frm.doc.posting_date,
+            to_date: me.frm.doc.posting_date
+          };
+          frappe.set_route("query-report", "Ashbee Stock Movement");
+        }, __("View"));
+      }
     }
 });
 
